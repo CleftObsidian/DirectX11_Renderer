67d841f8d176122dc6e11ef585ea068f47ac6886
CleftObsidian <walkpark@khu.ac.kr>
2022-04-17 23:43:27 +0900

    [LAB05] lab05 submitted

--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -96,7 +96,7 @@ INT WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
         return 0;
     }
 
-    std::shared_ptr<DysonCube> dysonCube = std::make_shared<DysonCube>("brickwall.dds");
+    std::shared_ptr<DysonCube> dysonCube = std::make_shared<DysonCube>("rainbow.dds");
     if (FAILED(game->GetRenderer()->AddRenderable(L"DysonCube", dysonCube)))
     {
         return 0;
